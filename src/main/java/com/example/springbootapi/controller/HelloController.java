2e0d73ccdd338ed4229b974150c56451e2eaea53
phamxuanloc0 <locpham2307@gmail.com>
2021-09-17 08:24:30 +0700

    CrudBase

--- src/main/java/com/example/springbootapi/controller/HelloController.java
+++ src/main/java/com/example/springbootapi/controller/HelloController.java
@@ -1,4 +1,12 @@
 package com.example.springbootapi.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+    @GetMapping("/")
+    public String helloWorld(){
+        return "Hello my friends";
+    }
 }
--- src/main/java/com/example/springbootapi/service/EmployeeServiceImpl.java
+++ src/main/java/com/example/springbootapi/service/EmployeeServiceImpl.java
@@ -1,34 +1,56 @@
 package com.example.springbootapi.service;
 
+import com.example.springbootapi.Repository.EmployeeRepository;
 import com.example.springbootapi.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService{
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
     @Override
     public Employee saveEmployee(Employee employee) {
-        return null;
+        return employeeRepository.save(employee);
     }
 
     @Override
     public List<Employee> getAllEmployees() {
-        return null;
+        List<Employee> lists= new ArrayList<Employee>();
+        employeeRepository.findAll().forEach(list1->lists.add(list1));
+        return lists;
     }
 
     @Override
     public Employee getEmployeeById(Long id) {
-        return null;
+        return employeeRepository.findById(id).get();
     }
 
     @Override
     public void deleteById(Long id) {
-
+        employeeRepository.deleteById(id);
     }
 
     @Override
     public Employee updateEmployeeById(Long id, Employee employee) {
-        return null;
+        Employee employeeDB= employeeRepository.findById(id).get();
+        if(Objects.nonNull(employee.getFirstName()) &&
+                !"".equalsIgnoreCase(employee.getFirstName())){
+            employeeDB.setFirstName(employee.getFirstName());
+        }
+        if(Objects.nonNull(employee.getLastName()) &&
+                !"".equalsIgnoreCase(employee.getLastName())){
+            employeeDB.setLastName(employee.getLastName());
+        }
+        if(Objects.nonNull(employee.getEmail()) &&
+                !"".equalsIgnoreCase(employee.getEmail())){
+            employeeDB.setEmail(employee.getEmail());
+        }
+        return employeeRepository.save(employeeDB);
     }
 }
